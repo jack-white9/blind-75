8eea010c1c85531164cfe8e6cb7ee6f9e9b1f684
Jack White <jfw793@gmail.com>
2023-07-10 22:12:49 +1000

    20. Valid Parentheses

---
+++ stacks/valid_parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for i in s:
+
+            if len(stack) == 0 or not (
+                i == ']' and stack[-1] == '[' or
+                i == ')' and stack[-1] == '(' or
+                i == '}' and stack[-1] == '{'
+            ):
+                stack.append(i)
+            else:
+                stack.pop()
+
+        return len(stack) == 0
+
